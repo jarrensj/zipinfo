cbd15af5fcd265315370231c076a715d908a0e9b
jarrensj <sanjose.jarren@gmail.com>
2019-05-28 17:08:51 -0700

    ionic integration

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        ZipInfo
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      My App
+    </ion-content>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
